e9bbc12b9d5414f071b14ac8a481baaa1ee6937c
XMrtN <108683218+XMrtN@users.noreply.github.com>
2023-02-08 22:22:56 -0300

    favicon, media queries e icono tema claro/oscuro

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -29,52 +29,55 @@ export class NavbarComponent implements OnInit {
   }
 
   onDarkMode():void {
-    const storedTheme = localStorage.getItem('theme')
+    const storedTheme = localStorage.getItem('theme');
     
     const getPreferredTheme = () => {
       if (storedTheme) {
-        return storedTheme
+        return storedTheme;
       }
   
-      return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+      return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
     }
   
     const setTheme = function (theme: string) {
       if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.documentElement.setAttribute('data-bs-theme', 'dark')
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
       } else {
-        document.documentElement.setAttribute('data-bs-theme', theme)
+        document.documentElement.setAttribute('data-bs-theme', theme);
       }
     }
   
-    setTheme(getPreferredTheme())
+    setTheme(getPreferredTheme());
   
     const showActiveTheme = (theme: string) => {
-      const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
+      const activeThemeIcon = document.querySelector('.dropdown-toggle i');
+      const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`);
+      const svgOfActiveBtn = btnToActive!.querySelector('i')!.className;
   
       document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-        element.classList.remove('active')
+        element.classList.remove('active');
       })
   
-      btnToActive!.classList.add('active')
+      btnToActive!.classList.add('active');
+      activeThemeIcon!.setAttribute('class', svgOfActiveBtn);
     }
   
     window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
       if (storedTheme !== 'light' && storedTheme !== 'dark') {
-        setTheme(getPreferredTheme())
+        setTheme(getPreferredTheme());
       }
     })
   
     window.addEventListener('DOMContentLoaded', () => {
-      showActiveTheme(getPreferredTheme())
+      showActiveTheme(getPreferredTheme());
   
       document.querySelectorAll('[data-bs-theme-value]')
         .forEach(toggle => {
           toggle.addEventListener('click', () => {
-            const theme = toggle.getAttribute('data-bs-theme-value')
-            localStorage.setItem('theme', theme!)
-            setTheme(theme!)
-            showActiveTheme(theme!)
+            const theme = toggle.getAttribute('data-bs-theme-value');
+            localStorage.setItem('theme', theme!);
+            setTheme(theme!);
+            showActiveTheme(theme!);
           })
         })
     })
